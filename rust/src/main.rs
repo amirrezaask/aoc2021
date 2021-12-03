2c929089cab3b780c4623a046b3e77e8aa3c619e
amirrezaask <raskarpour@gmail.com>
2021-12-03 10:30:18 +0330

    second day in rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,7 @@
 mod one;
+mod two;
 
 fn main() {
-    one::solve("1.txt");
+    // one::solve("1.txt");
+    two::solve("2.txt");
 }
---
+++ rust/src/two.rs
@@ -0,0 +1,76 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+struct Command {
+    action: String,
+    arg: i32,
+}
+
+fn solve_one(path: &str) {
+    let fd = File::open(path).unwrap();
+    let reader = BufReader::new(fd).lines();
+    let commands: Vec<Command> = reader
+        .filter_map(|line| {
+            if let Ok(s) = line {
+                let splitted = s.split(" ").collect::<Vec<&str>>();
+                Some(Command {
+                    action: splitted[0].to_string(),
+                    arg: splitted[1].to_string().parse::<i32>().unwrap(),
+                })
+            } else {
+                None
+            }
+        })
+        .collect();
+    let mut h = 0;
+    let mut d = 0;
+    for command in commands {
+        if command.action == "forward" {
+            h += command.arg;
+        } else if command.action == "down" {
+            d += command.arg;
+        } else if command.action == "up" {
+            d -= command.arg
+        }
+    }
+    println!("answer to day 2 first part is {}", d * h);
+}
+fn solve_two(path: &str) {
+    let fd = File::open(path).unwrap();
+    let reader = BufReader::new(fd).lines();
+    let commands: Vec<Command> = reader
+        .filter_map(|line| {
+            if let Ok(s) = line {
+                let splitted = s.split(" ").collect::<Vec<&str>>();
+                Some(Command {
+                    action: splitted[0].to_string(),
+                    arg: splitted[1].to_string().parse::<i32>().unwrap(),
+                })
+            } else {
+                None
+            }
+        })
+        .collect();
+    let mut h = 0;
+    let mut d = 0;
+    let mut a = 0;
+    for command in commands {
+        if command.action == "forward" {
+            h += command.arg;
+            d += a * command.arg;
+        } else if command.action == "down" {
+            a += command.arg;
+        } else if command.action == "up" {
+            a -= command.arg
+        }
+    }
+
+    println!("answer to day 2 second part is {}", d * h);
+}
+
+pub fn solve(path: &str) {
+    solve_one(path);
+    solve_two(path);
+}
