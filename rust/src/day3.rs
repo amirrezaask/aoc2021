1b4e2717911812573600bf042127c3349cad7e14
amirrezaask <raskarpour@gmail.com>
2021-12-05 12:17:39 +0330

    WIP: day3

--- rust/src/day3.rs
+++ rust/src/day3.rs
@@ -1,5 +1,9 @@
-use std::{fs::File, io::{BufReader, BufRead}, collections::HashMap};
 use std::str;
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 pub fn solve(path: &str) {
     solve_one(path);
@@ -9,12 +13,12 @@ pub fn solve(path: &str) {
 fn solve_one(path: &str) {
     let fd = File::open(path).unwrap();
     let reader = BufReader::new(fd).lines();
-    let numbers: Vec<String> = reader.filter_map(|line| 
-        match line {
+    let numbers: Vec<String> = reader
+        .filter_map(|line| match line {
             Ok(l) => Some(l),
             Err(_) => None,
-        } 
-    ).collect();
+        })
+        .collect();
     let number_len = numbers[0].len();
     // map of pos => (0's count, 1's count)
     let mut db: HashMap<i32, (i32, i32)> = HashMap::new();
@@ -24,11 +28,11 @@ fn solve_one(path: &str) {
             match db.get(&i) {
                 Some(v) => {
                     if n == '0' {
-                        db.insert(i, (v.0+1, v.1));
+                        db.insert(i, (v.0 + 1, v.1));
                     } else {
-                        db.insert(i, (v.0, v.1+1));
+                        db.insert(i, (v.0, v.1 + 1));
                     }
-                },
+                }
                 None => {
                     if n == '0' {
                         db.insert(i, (1, 0));
@@ -36,8 +40,8 @@ fn solve_one(path: &str) {
                         db.insert(i, (0, 1));
                     }
                 }
-            } 
-        } 
+            }
+        }
     }
     let mut gamma_rate_str = String::new();
     let mut epsilon_rate_str = String::new();
@@ -51,12 +55,60 @@ fn solve_one(path: &str) {
             gamma_rate_str.push('1');
         }
     }
-    let gamma_rate =  isize::from_str_radix(&gamma_rate_str, 2).unwrap();
+    let gamma_rate = isize::from_str_radix(&gamma_rate_str, 2).unwrap();
     let epsilon_rate = isize::from_str_radix(&epsilon_rate_str, 2).unwrap();
-    println!("answer to day 3 part 1 is {}", gamma_rate*epsilon_rate)
-    
+    println!("answer to day 3 part 1 is {}", gamma_rate * epsilon_rate)
+}
+
+fn most_common_bit_at(list: &Vec<String>, pos: usize) -> i8 {
+    let acc = list.into_iter().fold(0, |counter, elem| {
+        match elem.chars().nth(pos.into()).unwrap() {
+            '1' => counter + 1,
+            '0' => counter - 1,
+            _ => counter,
+        }
+    });
+    match acc > 0 {
+        true => 1,
+        false => 0,
+    }
+}
+
+fn meets_bit_criteria_oxygen(number: String, most_commons: Vec<char>) -> bool {
+    for i in 0..number.len() {
+        if number.chars().nth(i).unwrap() != most_commons[i] {
+            return false;
+        }
+    }
+    true
+}
+fn meets_bit_criteria_co2(number: String, most_commons: Vec<char>) -> bool {
+    for i in 0..number.len() {
+        if number.chars().nth(i).unwrap() != most_commons[i] {
+            return true;
+        }
+    }
+    false
 }
 
 fn solve_two(path: &str) {
+    let fd = File::open(path).unwrap();
+    let reader = BufReader::new(fd).lines();
+    let numbers: Vec<String> = reader
+        .filter_map(|line| match line {
+            Ok(l) => Some(l),
+            Err(_) => None,
+        })
+        .collect();
 
+    let numbers_len = numbers[0].len();
+    let mut most_common_bits: Vec<char> = Vec::new();
+    for i in 0..numbers_len {
+        let most_common = most_common_bit_at(&numbers, i);
+        most_common_bits.push(match most_common {
+            1 => '1',
+            0 => '0',
+            _ => '1',
+        })
+    }
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,5 +5,5 @@ mod day3;
 fn main() {
     day1::solve("1.txt");
     day2::solve("2.txt");
-    day3::solve("3.txt");
+    day3::solve("3.test");
 }
